2f262ee3a35d262d5a516f3da27f117e6b1460ad
Kevin Kügler <kevinkuegler@outlook.com>
2021-02-04 09:26:43 +0100

    Initial Code Classes
    - Move Task description
    - Rename ecgdat.sec to ecg.dat

---
+++ Electrocardiogram-Core/Source/EKG.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Signal.hpp"
+
+class EKG : public Signal
+{
+public:
+    void ExportToCSV();
+    uint32_t GetHeartbeatCount() const;
+    uint32_t GetHeatbeatsPerMinute() const;
+
+private:
+    uint32_t _samplingIntervalMs = 5;
+};
---
+++ Electrocardiogram-Core/Source/Signal.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class Signal
+{
+public:
+    Signal() = default;
+
+    void ReadFromCSV(const std::string& filename);
+    void WriteToCSV(const std::string& filename);
+    uint64_t GetCount() const;
+    uint16_t GetMinimum() const;
+    uint16_t GetMaximum() const;
+    uint64_t GetSum() const;
+    uint64_t GetAverage() const;
+    uint64_t GetStandardDeviation() const;
+
+    void Smooth();
+
+    // TODO: Operator Overload
+
+private:
+    std::vector<uint16_t> _readings;
+};
